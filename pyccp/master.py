41d7d7164955fe7bd05130362488c76a751a1214
Christoph Schueler <cpu12.gems@googlemail.com>
2016-11-06 14:58:49 +0000

    Add some tests.

--- pyccp/master.py
+++ pyccp/master.py
@@ -31,6 +31,9 @@ import struct
 from pyccp import ccp
 
 
+MTA0 = 0
+MTA1 = 1
+
 class Master(ccp.CRO):
 
     def __init__(self, transport):
@@ -54,32 +57,37 @@ class Master(ccp.CRO):
     def getCCPVersion(self, canID, major = 2, minor = 1):
         self.sendCRO(canID, ccp.CommandCodes.GET_CCP_VERSION, self.ctr, major, minor)
 
-    def exchangeId(self, canID):
-        pass    # EXCHANGE_ID
+    def exchangeId(self, canID, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0):
+        self.sendCRO(canID, ccp.CommandCodes.EXCHANGE_ID, self.ctr, b0, b1, b2, b3, b4, b5)
 
-    def setMta(self, canID):
-        pass    # SET_MTA
+    def setMta(self, canID, address, addressExtension = 0x00, mta = MTA0):
+        address = struct.pack(">L", address)
+        self.sendCRO(canID, ccp.CommandCodes.SET_MTA, self.ctr, mta, addressExtension, *address)
 
-    def dnload(self, canID):
-        pass    # DNLOAD
+    def dnload(self, canID, size, data):
+        self.sendCRO(canID, ccp.CommandCodes.DNLOAD, self.ctr, size, *data)
 
-    def upload(self, canID):
-        pass    # UPLOAD
+    def upload(self, canID, size):
+        self.sendCRO(canID, ccp.CommandCodes.UPLOAD, self.ctr, size)
 
-    def getDaqSize(self, canID):
-        pass    # GET_DAQ_SIZE
+    def getDaqSize(self, canID, daqListNumber, address):
+        address = struct.pack(">L", address)
+        self.sendCRO(canID, ccp.CommandCodes.GET_DAQ_SIZE, self.ctr, daqListNumber, 0x00, *address)
 
-    def setDaqPtr(self, canID):
-        pass    # SET_DAQ_PTR
+    def setDaqPtr(self, canID, daqListNumber, odtNumber, elementNumber):
+        self.sendCRO(canID, ccp.CommandCodes.SET_DAQ_PTR, self.ctr, daqListNumber, odtNumber, elementNumber)
 
-    def writeDaq(self, canID):
-        pass    # WRITE_DAQ
+    def writeDaq(self, canID, elementSize, addressExtension, address):
+        address = struct.pack(">L", address)
+        self.sendCRO(canID, ccp.CommandCodes.WRITE_DAQ, self.ctr, elementSize, addressExtension, *address)
 
-    def startStop(self, canID):
-        pass    # START_STOP
+    def startStop(self, canID, mode, daqListNumber, lastOdtNumber, eventChannel, ratePrescaler):
+        ratePrescaler = struct.pack(">H", ratePrescaler)
+        self.sendCRO(canID, ccp.CommandCodes.START_STOP, self.ctr, mode, daqListNumber, lastOdtNumber, eventChannel, *ratePrescaler)
 
-    def disconnect(self, canID):
-        pass    # DISCONNECT
+    def disconnect(self, canID, permanent, address):
+        address = struct.pack("<H", address)
+        self.sendCRO(canID, ccp.CommandCodes.DISCONNECT, self.ctr, permanent, 0x00, *address)
 
     ##
     ## Optional Commands.
@@ -91,9 +99,6 @@ class Master(ccp.CRO):
         pass
 
     def shortUp(self, canID, size, address, addressExtension):
-        """
-        0x0f    0x23 0x04 0x00 0x12 0x34 0x56 0x 78
-        """
         pass
 
     def startStopAll(self, canID):
--- pyccp/tests/testMaster.py
+++ pyccp/tests/testMaster.py
@@ -11,7 +11,6 @@ class TestMaster(unittest.TestCase):
     def testConnect(self):
         transport = ccp.Transport()
         master = Master(transport)
-
         master.ctr = 0x27
         master.connect(0x7E1, 0x39)
         self.assertEqual(str(master.transport.message), "07E1  01 27 39 00 00 00 00 00")
@@ -23,6 +22,68 @@ class TestMaster(unittest.TestCase):
         master.getCCPVersion(0x7e1)
         self.assertEqual(str(master.transport.message), '07E1  1B 27 02 01 00 00 00 00')
 
+    def testExchangeID(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.exchangeId(0x7e1)
+        self.assertEqual(str(master.transport.message), '07E1  17 23 00 00 00 00 00 00')
+
+    def testSetMta(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.setMta(0x7e1, 0x34002000, 0x02, 0x00)
+        self.assertEqual(str(master.transport.message), '07E1  02 23 00 02 34 00 20 00')
+
+    def testDnload(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.dnload(0x7e1, 5, bytearray([0x10, 0x11, 0x12, 0x13, 0x14]))
+        self.assertEqual(str(master.transport.message), '07E1  03 23 05 10 11 12 13 14')
+
+    def testUpload(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.upload(0x7e1, 4)
+        self.assertEqual(str(master.transport.message), '07E1  04 23 04 00 00 00 00 00')
+
+    def testUpload(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.getDaqSize(0x7e1, 3, 0x01020304)
+        self.assertEqual(str(master.transport.message), '07E1  14 23 03 00 01 02 03 04')
+
+    def testSetDaqPtr(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.setDaqPtr(0x7e1, 3, 5, 2)
+        self.assertEqual(str(master.transport.message), '07E1  15 23 03 05 02 00 00 00')
+
+    def testWriteDaqPtr(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.writeDaq(0x7e1, 0x02, 0x01, 0x02004200)
+        self.assertEqual(str(master.transport.message), '07E1  16 23 02 01 02 00 42 00')
+
+    def testStartStopPtr(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.startStop(0x7e1, 0x01, 0x03, 0x07, 0x02, 0x01)
+        self.assertEqual(str(master.transport.message), '07E1  06 23 01 03 07 02 00 01')
+
+    def testDisconnect(self):
+        transport = ccp.Transport()
+        master = Master(transport)
+        master.ctr = 0x23
+        master.disconnect(0x7e1, 0x00, 0x0208)
+        self.assertEqual(str(master.transport.message), '07E1  07 23 00 00 08 02 00 00')
 
 if __name__ == '__main__':
     unittest.main()

a930433d2673744b5d7cfa3e8fe6968b5a860b9e
Christoph Schueler <cpu12.gems@googlemail.com>
2016-11-07 06:02:15 +0000

    Refactored tests.

--- pyccp/tests/testMaster.py
+++ pyccp/tests/testMaster.py
@@ -8,82 +8,51 @@ from pyccp.master import Master
 
 class TestMaster(unittest.TestCase):
 
-    def testConnect(self):
+    def setUp(self):
         transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x27
-        master.connect(0x7E1, 0x39)
-        self.assertEqual(str(master.transport.message), "07E1  01 27 39 00 00 00 00 00")
+        self.master = Master(transport)
+        self.master.ctr = 0x27
+
+    def tearDown(self):
+        del self.master
+
+    def runTest(self, func, expectedResult, *params):
+        getattr(self.master, func)(*params)
+        result = str(self.master.transport.message)
+        self.assertEqual(result, expectedResult)
+
+    def testConnect(self):
+        self.runTest("connect", "07E1  01 27 39 00 00 00 00 00", 0x7E1, 0x39)
 
     def testGetCCPVersion(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x27
-        master.getCCPVersion(0x7e1)
-        self.assertEqual(str(master.transport.message), '07E1  1B 27 02 01 00 00 00 00')
+        self.runTest("getCCPVersion", "07E1  1B 27 02 01 00 00 00 00", 0x7E1)
 
     def testExchangeID(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.exchangeId(0x7e1)
-        self.assertEqual(str(master.transport.message), '07E1  17 23 00 00 00 00 00 00')
+        self.runTest("exchangeId", "07E1  17 27 00 00 00 00 00 00", 0x7E1)
 
     def testSetMta(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.setMta(0x7e1, 0x34002000, 0x02, 0x00)
-        self.assertEqual(str(master.transport.message), '07E1  02 23 00 02 34 00 20 00')
+        self.runTest("setMta", "07E1  02 27 00 02 34 00 20 00", 0x7E1, 0x34002000, 0x02, 0x00)
 
     def testDnload(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.dnload(0x7e1, 5, bytearray([0x10, 0x11, 0x12, 0x13, 0x14]))
-        self.assertEqual(str(master.transport.message), '07E1  03 23 05 10 11 12 13 14')
+        self.runTest("dnload", "07E1  03 27 05 10 11 12 13 14", 0x7e1, 5, bytearray([0x10, 0x11, 0x12, 0x13, 0x14]))
 
     def testUpload(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.upload(0x7e1, 4)
-        self.assertEqual(str(master.transport.message), '07E1  04 23 04 00 00 00 00 00')
+        self.runTest("upload", "07E1  04 23 04 00 00 00 00 00", 0x7e1, 4)
 
     def testUpload(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.getDaqSize(0x7e1, 3, 0x01020304)
-        self.assertEqual(str(master.transport.message), '07E1  14 23 03 00 01 02 03 04')
+        self.runTest("getDaqSize", "07E1  14 27 03 00 01 02 03 04", 0x7e1, 3, 0x01020304)
 
     def testSetDaqPtr(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.setDaqPtr(0x7e1, 3, 5, 2)
-        self.assertEqual(str(master.transport.message), '07E1  15 23 03 05 02 00 00 00')
+        self.runTest("setDaqPtr", "07E1  15 27 03 05 02 00 00 00", 0x7e1, 3, 5, 2)
 
     def testWriteDaqPtr(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.writeDaq(0x7e1, 0x02, 0x01, 0x02004200)
-        self.assertEqual(str(master.transport.message), '07E1  16 23 02 01 02 00 42 00')
+        self.runTest("writeDaq", "07E1  16 27 02 01 02 00 42 00", 0x7e1, 0x02, 0x01, 0x02004200)
 
     def testStartStopPtr(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.startStop(0x7e1, 0x01, 0x03, 0x07, 0x02, 0x01)
-        self.assertEqual(str(master.transport.message), '07E1  06 23 01 03 07 02 00 01')
+        self.runTest("startStop", "07E1  06 27 01 03 07 02 00 01", 0x7e1, 0x01, 0x03, 0x07, 0x02, 0x01)
 
     def testDisconnect(self):
-        transport = ccp.Transport()
-        master = Master(transport)
-        master.ctr = 0x23
-        master.disconnect(0x7e1, 0x00, 0x0208)
-        self.assertEqual(str(master.transport.message), '07E1  07 23 00 00 08 02 00 00')
+        self.runTest("disconnect", "07E1  07 27 00 00 08 02 00 00", 0x7e1, 0x00, 0x0208)
 
 if __name__ == '__main__':
     unittest.main()

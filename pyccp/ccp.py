5cf8d22d0f3ca15c9b71022c62e04bebe7bcb511
Christoph Schueler <cpu12.gems@googlemail.com>
2016-11-21 13:14:20 +0100

    Added enumeration 'DTOType'.

--- pyccp/ccp.py
+++ pyccp/ccp.py
@@ -34,8 +34,6 @@ MAX_DTO = 0x0008
 
 
 class CommandCodes(enum.IntEnum):
-    """
-    """
 
     # Mandatory Commands.
     CONNECT             = 0x01
@@ -88,6 +86,11 @@ class ReturnCodes(enum.IntEnum):
     OVERLOAD                        = 0x34    # C3 FAULT
     ACCESS_LOCKED                   = 0x35    # C3 FAULT
     RESOURCE_FUNCTION_NOT_AVAILABLE = 0x36    # C3 FAULT
+
+
+class DTOType(enum.IntEnum):
+    COMMAND_RETURN_MESSAGE  = 255
+    EVENT_MESSAGE           = 254
 
 
 class State(enum.IntEnum):
@@ -174,6 +177,7 @@ class Transport(object):
 
     def send(self, canID, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0, b7 = 0):
         self.message = CANMessageObject(canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7)))
+        #print("Sending: {}".format(self.message))
 
     def receive(self, canID, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0, b7 = 0):
         self.message = CANMessageObject(canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7)))
